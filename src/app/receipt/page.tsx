42314ce2cb0b537e6c47f7d31ccc8dd82d53600b
Emre Agaliev <emreagaliev@gmail.com>
2025-05-27 10:30:48 +0500

    dd

--- src/app/receipt/page.tsx
+++ src/app/receipt/page.tsx
@@ -1,9 +1,6 @@
+// app/receipt/page.tsx (серверный)
 import ReceiptClient from "./ReceiptClient"
 
-export default function ReceiptPage({
-  searchParams
-}: {
-  searchParams: { [key: string]: string | string[] | undefined }
-}) {
+export default function ReceiptPage({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) {
   return <ReceiptClient searchParams={searchParams} />
 }
