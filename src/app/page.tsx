1763134cb81a6ba4bd3ec06d6210796d3ef048f3
Emre Agaliev <emreagaliev@gmail.com>
2025-05-27 11:14:32 +0500

    emre

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { useRouter } from "next/navigation"
 import { useState } from "react"
 import { Button } from "@/components/ui/button"
 import {
@@ -14,17 +13,8 @@ import {
 } from "@/components/ui/select"
 
 export default function Home() {
-  const router = useRouter()
   const [status, setStatus] = useState<string | null>(null)
 
-  const handleGenerate = () => {
-    if (!status) return alert("Выберите статус перед генерацией чека")
-
-    const date = new Date().toLocaleString()
-    const id = Math.random().toString(36).slice(2, 10).toUpperCase()
-
-    router.push(`/receipt?status=${status}&date=${encodeURIComponent(date)}&id=${id}`)
-  }
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -38,13 +28,13 @@ export default function Home() {
               <SelectLabel>Статус</SelectLabel>
               <SelectItem value="Оплачен">Оплачен</SelectItem>
               <SelectItem value="Возврат">Возврат</SelectItem>
-              <SelectItem value="Отмена">Аванс</SelectItem>
+              <SelectItem value="Отмена"></SelectItem>
               <SelectItem value="Кредит">Кредит</SelectItem>
             </SelectGroup>
           </SelectContent>
         </Select>
 
-        <Button onClick={handleGenerate}>
+        <Button>
           Сгенерировать чек
         </Button>
       </main>
